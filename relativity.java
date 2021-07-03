e94eee91388bcc4065d4265c9b5fd79a4588a613
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-07-03 16:20:33 +0530

    Create relativity.java

---
+++ relativity.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int n=s.nextInt();//testcase
+		    for (int i=0;i<n;i++){
+		        int a=s.nextInt();//take numbers
+		        int b=s.nextInt();
+		        int c=b*b;
+		        int d=2*a;
+		        System.out.println(c/d);
+		      
+		    }
+		}catch(Exception e){
+	        
+	    }
+	}
+}
